9f1a6eacf6fa944c7d033c41703d2a04ea8264ea
SYL <1061300519@qq.com>
2019-04-05 02:05:21 +0800

    code test

--- App1/MYSQL_DB/MysqlDataBase.cs
+++ App1/MYSQL_DB/MysqlDataBase.cs
@@ -7,9 +7,12 @@ namespace App1.MYSQL_DB
     {
         public Boolean tetst_db()
         {
-            string query = "select u.current_login as id ,p.name as userName,p.phone" +
-                " from sys_user u left join person_info p " +
-                "on p.id = u.persion_id limit 2";
+            string query = "select u.current_login as id ," +
+                            " p.name as userName," +
+                            " p.phone" +
+                            " from sys_user u " +
+                            " left join person_info p " +
+                            " on p.id = u.persion_id limit 2";
             MySqlConnection myConnection = new MySqlConnection("server=localhost;user id=root;password=123456;database=jims-his");
             MySqlCommand myCommand = new MySqlCommand(query, myConnection);
             myConnection.Open();
